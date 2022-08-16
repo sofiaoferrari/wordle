5aacb38e4904e4457dbf21aa22c2871bb6f2122a
Sofia <65925889+sofiaoferrari@users.noreply.github.com>
2022-08-16 18:13:58 -0300

    tests passed

--- wordle.cpp
+++ wordle.cpp
@@ -1,7 +1,25 @@
 #include "funciones.h"
 
+#include <iostream>
+
 using namespace std;
 
 int main(){
-    // completar
+
+    string nombre_archivo = "../listado.txt";
+    vector<string> lista = cargar_listado(nombre_archivo);
+
+    int nro_random = 1 + rand()/((RAND_MAX + 1u)/36);
+    string palabra_random = lista.at(nro_random);
+
+    cout << "Ingrese un intento de palabra: ";
+    string intento_usuario;
+    cin >> intento_usuario;
+    cout << endl;
+
+    if (intento_valido(intento_usuario, palabra_random, lista)){
+        vector<EstadoLetra> evaluacion = evaluar_intento(intento_usuario, palabra_random);
+        string resultado = respuesta_en_string(evaluacion);
+        cout << resultado << endl;
+    } else cout << "Intento invalido" << endl;
 }

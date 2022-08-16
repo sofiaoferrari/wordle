89c4b8c62325b7c9109145dfe8c81c4ce5347f12
Sofia <65925889+sofiaoferrari@users.noreply.github.com>
2022-08-16 18:09:45 -0300

    Archivos iniciales

---
+++ funciones.cpp
@@ -0,0 +1,29 @@
+#include "funciones.h"
+#include <vector>
+#include <string>
+
+using namespace std;
+
+vector<string> cargar_listado(const string & nombre_archivo){
+    // completar
+
+    return vector<string>();
+}
+
+bool intento_valido(const string & intento, const string & palabra_secreta, const vector<string> &listado){
+    // completar
+
+    return false;
+}
+
+vector<EstadoLetra> evaluar_intento(const string & intento, const string & palabra_secreta){
+    // completar
+
+    return vector<EstadoLetra>();
+}
+
+string respuesta_en_string(const vector<EstadoLetra> & respuesta){
+    // completar
+
+    return "";
+}
---
+++ wordle.cpp
@@ -0,0 +1,7 @@
+#include "funciones.h"
+
+using namespace std;
+
+int main(){
+    // completar
+}

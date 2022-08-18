4deb6413aca51d85e8e7a1bec23241ae94679660
Sofia <65925889+sofiaoferrari@users.noreply.github.com>
2022-08-18 20:51:06 -0300

    Documentada

--- funciones.cpp
+++ funciones.cpp
@@ -7,27 +7,24 @@
 
 using namespace std;
 
-/**
- * @brief toma el nombre de un archivo que contiene una palabra por 
+/*
+ * Toma el nombre de un archivo que contiene una palabra por 
  * línea y devuelve un vector de strings con todas las palabras tal 
  * como están en el archivo.
- * 
- * @param nombre_archivo 
- * @return vector<string> 
  */
 vector<string> cargar_listado(const string & nombre_archivo){
     ifstream archivo (nombre_archivo);
 
     if (archivo.good() == false) {
-        cout << "No se pudo abrir el archivo " << nombre_archivo << endl;
-        exit(1);
+        cout << "Ups! No se pudo abrir el archivo '" << nombre_archivo << "'" << endl;
+        exit(1); //sale si el archivo no se pudo abrir
     }
 
     vector<string> palabras = {};
     string linea;
     while (!archivo.eof()){
         getline(archivo, linea);
-        if (!linea.empty())
+        if (!linea.empty()) //saltea las lineas vacias
             palabras.push_back(linea);
     }
     
@@ -35,30 +32,24 @@ vector<string> cargar_listado(const string & nombre_archivo){
 }
 
 
-/**
- * @brief toma un intento, una palabra secreta y un listado de palabras e indica si 
+/*
+ * Toma un intento, una palabra secreta y un listado de palabras e indica si 
  * el intento es legal o no. Un intento es legal si tiene la misma longitud que la 
  * palabra secreta y si es una palabra reconocida en el listado de palabras.
- * 
- * @param intento 
- * @param palabra_secreta 
- * @param listado 
- * @return true 
- * @return false 
  */
 bool intento_valido(const string & intento, const string & palabra_secreta, const vector<string> &listado){
-    if (intento.size() != palabra_secreta.size())
-        return false;
-
-    for (string palabra : listado) {
-        if (intento == palabra) return true;
+    if (intento.size() == palabra_secreta.size()) { //misma cantidad de caracteres
+        for (string palabra : listado) {
+            if (intento == palabra) //la palabra se encuentra en el listado
+                return true;
+        }
     }
 
     return false;
 }
 
-/**
- * @brief toma un intento y una palabra secreta, y devuelve un vector con la 
+/*
+ * Toma un intento y una palabra secreta, y devuelve un vector con la 
  * respuesta para cada una de las letras del intento. Una letra del intento puede 
  * estar en el LugarCorrect si la palabra secreta tiene esa misma letra en esa 
  * posición, puede estar en un LugarIncorrecto si la palabra secreta contiene esa 
@@ -67,20 +58,20 @@ bool intento_valido(const string & intento, const string & palabra_secreta, cons
  * Por ejemplo: si la palabra secreta es "bolsa" y el intento es "aviso", se espera 
  * que esta función devuelva el vector {LugarIncorrecto, NoPresente, NoPresente, 
  * LugarCorrecto, LugarIncorrecto}.
- * 
- * @param intento 
- * @param palabra_secreta 
- * @return vector<EstadoLetra> 
  */
 vector<EstadoLetra> evaluar_intento(const string & intento, const string & palabra_secreta){
     vector<EstadoLetra> estado;
     
     for (char letra : intento) {
-        if (palabra_secreta.find(letra) == intento.find(letra)) 
+        //utilizo la funcion .find() para buscar la posicion de cada caracter de la 
+        //palabra intento en la palabra secreta y ver si coinciden las posiciones.
+        //(sabiendo que devuelve -1 en la caso de que no exista la posicion)
+
+        if (palabra_secreta.find(letra) == intento.find(letra)) //coincide posicion
             estado.push_back(EstadoLetra::LugarCorrecto);
-        else if (palabra_secreta.find(letra) != -1) 
+        else if (palabra_secreta.find(letra) != -1) //contiene la letra pero en otra posicion
             estado.push_back(EstadoLetra::LugarIncorrecto);
-        else if (palabra_secreta.find(letra) == -1) 
+        else if (palabra_secreta.find(letra) == -1) //no contiene la letra
             estado.push_back(EstadoLetra::NoPresente);
     }
 
@@ -88,7 +79,7 @@ vector<EstadoLetra> evaluar_intento(const string & intento, const string & palab
 }
 
 /**
- * @brief toma el vector de EstadoLetra que devuelve evaluar_intento 
+ * Toma el vector de EstadoLetra que devuelve evaluar_intento 
  * y devuelve un string que se puede imprimir por pantalla para indicar 
  * esa respuesta al usuario. Se espera que LugarCorrecto se indique con 
  * el caracter "+", LugarIncorrecto con el caracter "-", y NoPresente con 
@@ -96,9 +87,6 @@ vector<EstadoLetra> evaluar_intento(const string & intento, const string & palab
  * Por ejemplo: si el vector en entrada es {LugarIncorrecto, NoPresente, 
  * NoPresente, LugarCorrecto, LugarIncorrecto} se espera que el string de 
  * salida sea "-XX+-".
- * 
- * @param respuesta 
- * @return string 
  */
 string respuesta_en_string(const vector<EstadoLetra> & respuesta){
     string resultado = "";

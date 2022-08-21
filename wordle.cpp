5666e56bd5775aabb1e0e7e78fd3dad53825b816
Sofia <65925889+sofiaoferrari@users.noreply.github.com>
2022-08-21 14:23:57 -0300

    Entregado

--- wordle.cpp
+++ wordle.cpp
@@ -10,38 +10,40 @@ int main(){
     cout << "W O R D L E" << endl << endl;
 
     int intentos = 6;
-    bool won = false;
-    string nombre_archivo = "../vacion.txt";
-    vector<string> lista = cargar_listado(nombre_archivo);
+    bool gano = false; //utilizo una bandera para cuando gana el usuario
+    string nombre_archivo = "../listado.txt";
+    vector<string> listado = cargar_listado(nombre_archivo);
 
-    if (lista.empty()) { //salida si listado vacio
+    if (listado.empty()) { //si el listado se encuentra vacio sale
         cout << "Ups! Hubo un error con el archivo." << endl;
         return -1; //error
     }
+    int nro_random;
 
+    //tomo un numero al azar utilizando .rand() 
     srand(time(nullptr));
-    int nro_random = 1 + rand()/((RAND_MAX + 1u)/lista.size());
-    string palabra_random = lista.at(nro_random);
-
+    nro_random = rand()/((RAND_MAX + 1u)/listado.size()); //lo divido por la cantidad de elementos del listado para que me de un numero entre 0 y la cantidad de palabras contenidas en el vector
+    
+    string palabra_random = listado.at(nro_random); //uso la funcion .at(posicion) de <string> para obtener la palabra del vector en la posicion random obtenida anteriormente
     cout << "El juego esta por comenzar! Tienes 6 intentos :)" << endl;
-    cout << "Recuerda que la palabra debe ser de 5 caracteres. " << endl;
+    cout << "Recuerda que la palabra es de 5 letras. " << endl;
 
-    while(intentos > 0) {
+    while(intentos > 0 && !gano) { //itera mientras el usuario tenga intentos y no haya ganado
 
         cout << endl << "Ingrese un intento de palabra: ";
         string intento_usuario;
-        getline(cin, intento_usuario); //utilizo getline para el input del usuario para no obtener errores por espacios
+        getline(cin, intento_usuario); //utilizo getline para el input del usuario para no obtener errores por espacios ' '
         cout << endl;
 
-        if (intento_valido(intento_usuario, palabra_random, lista)){
+        if (intento_valido(intento_usuario, palabra_random, listado)){
             vector<EstadoLetra> evaluacion = evaluar_intento(intento_usuario, palabra_random);
             string resultado = respuesta_en_string(evaluacion);
-            if (resultado == "+++++") {
+            if (resultado == "+++++") { //si todas las letras estan bien ubicadas
                 cout << "La palabra '" << intento_usuario << "' es la correcta." << endl;
                 cout << "Ganaste!!" << endl;
-                intentos = 0;
-                won = true;
+                gano = true; //adivino -> bandera true
             } else {
+                cout << intento_usuario << endl;
                 cout << resultado << endl;
                 intentos--;
                 cout << "Intentos restantes: " << intentos << endl; 
@@ -52,8 +54,10 @@ int main(){
             cout << "Intentos restantes: " << intentos << endl; 
         }
 
-        if (intentos ==  0 && !won) 
+        if (intentos ==  0 ) { //si el usuario se quedo sin intentos
+            cout << endl;
             cout << "Ups! Te quedaste sin intentos. Vuelve a jugar." << endl;
+        }
     }
 
     return 0;
